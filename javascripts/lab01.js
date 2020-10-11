ed29c647a4227be710bc87082114f8fd2ee1f42c
이대엽(Lee Dae Yeop) <42068811+tony6303@users.noreply.github.com>
2020-10-11 16:53:15 +0900

    Update lab01.js

--- javascripts/lab01.js
+++ javascripts/lab01.js
@@ -1,13 +1,11 @@
+function f1(x){
+	x.style.background='orange';
+	x.style.width = "500px";
+	x.style.height = "100px"
+}
+function f2(x){
+	x.style.background='cyan';
+	x.style.width = "1000px";
+	x.style.height = "100px"
+}
 
-<script type="text/javascript">
-		function f1(x){
-			x.style.background='orange';
-			x.style.width = "500px";
-			x.style.height = "100px"
-		}
-		function f2(x){
-			x.style.background='cyan';
-			x.style.width = "1000px";
-			x.style.height = "100px"
-		}
-	</script>
